ea5cf35a14e13e9ed9ce51984f002e3be9e14c66
Paul Michael Reilly <pmr@pajato.com>
2018-08-14 03:01:14 -0400

    Initial Xcode project setup
    Rationale:
    =========
    Set up the project to a baseline level where the project environment
    has been verified to ensure that all initial tests pass and all
    initial errors have been resolved. At this point the project is ready
    for TDD to commence.
    
    File changes:
    ============
    new file:   ArgusCoreAuth/ArgusCoreAuth.xcodeproj/project.pbxproj
    new file:   ArgusCoreAuth/ArgusCoreAuth.xcodeproj/project.xcworkspace/contents.xcworkspacedata
    new file:   ArgusCoreAuth/ArgusCoreAuth.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist
    new file:   ArgusCoreAuth/ArgusCoreAuth/ArgusCoreAuth.h
    new file:   ArgusCoreAuth/ArgusCoreAuth/Info.plist
    new file:   ArgusCoreAuth/ArgusCoreAuthTests/ArgusCoreAuthTests.swift
    new file:   ArgusCoreAuth/ArgusCoreAuthTests/Info.plist
    new file:   context.png
    new file:   core-authentication.png
    new file:   notes.org
    
    - Summary: create and add the initial baseline project files.

---
+++ ArgusCoreAuth/ArgusCoreAuth/ArgusCoreAuth.h
@@ -0,0 +1,19 @@
+//
+//  ArgusCoreAuth.h
+//  ArgusCoreAuth
+//
+//  Created by Paul Reilly on 8/14/18.
+//  Copyright © 2018 pajato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArgusCoreAuth.
+FOUNDATION_EXPORT double ArgusCoreAuthVersionNumber;
+
+//! Project version string for ArgusCoreAuth.
+FOUNDATION_EXPORT const unsigned char ArgusCoreAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArgusCoreAuth/PublicHeader.h>
+
+
